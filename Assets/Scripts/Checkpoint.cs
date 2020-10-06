557b5029e625c5562bf3ea1e76ec9ef6065d8d6d
Hunter <hunt3rchu@gmail.com>
2020-10-06 15:53:13 -0400

    added checkpoints with particle effects

---
+++ Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    public DeathPlane deathPlane;
+    public ParticleSystem particles;
+    ParticleSystem.MainModule main;
+    public void Awake()
+    {
+        main = particles.GetComponent<ParticleSystem>().main;
+    }
+
+    private void OnTriggerEnter()
+    {
+        main.startColor = Color.green;
+        deathPlane.checkpoint = gameObject;
+    }
+
+    private void OnTriggerExit()
+    {
+        main.startColor = Color.cyan;
+    }
+}
