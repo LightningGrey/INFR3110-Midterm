c4bf6346182445d6afa7981955f522107500b526
Hunter <hunt3rchu@gmail.com>
2020-10-07 02:16:55 -0400

    branch save to test rotation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public Animator anim;
+    public bool moveFlag = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
@@ -15,19 +18,34 @@ public class PlayerMovement : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.W))
         {
+            moveFlag = true;
             transform.Translate(new Vector3(0.0f, 0.0f, 3f * Time.deltaTime));
         }
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Translate(new Vector3(-3f * Time.deltaTime, 0.0f, 0.0f));
+            moveFlag = true;
+            transform.Translate(new Vector3(3f * Time.deltaTime, 0.0f, 0.0f));
         }
         if (Input.GetKey(KeyCode.S))
         {
-            transform.Translate(new Vector3(0.0f, 0.0f, -3f * Time.deltaTime));
+            moveFlag = true;
+            transform.Translate(new Vector3(0.0f, 0.0f, 3f * Time.deltaTime));
         }
         if (Input.GetKey(KeyCode.D))
         {
+            moveFlag = true;
             transform.Translate(new Vector3(3f * Time.deltaTime, 0.0f, 0.0f));
         }
+
+        if (moveFlag)
+        {
+            anim.SetBool("walk", true);
+        } else
+        {
+            anim.SetBool("walk", false);
+        }
+
+        moveFlag = false;
+
     }
 }
--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Midterm for Game Engines. A maze game that utilizes DLL capabilities.
 Assets used:
 https://assetstore.unity.com/packages/3d/environments/stylized-hand-painted-dungeon-free-173934
 https://assetstore.unity.com/packages/3d/characters/humanoids/fantasy/strong-knight-83586
+https://assetstore.unity.com/packages/3d/animations/basic-motions-free-pack-154271
