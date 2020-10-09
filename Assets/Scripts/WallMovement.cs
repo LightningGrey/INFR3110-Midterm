f82858a7d3943bb7611979c9d503990e7698363e
Hunter <hunt3rchu@gmail.com>
2020-10-09 00:04:50 -0400

    areas 2 and 3 complete

---
+++ Assets/Scripts/WallMovement.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Runtime.CompilerServices;
+using Unity.Profiling;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class WallMovement : MonoBehaviour
+{
+    //direction
+    //negative or positive;
+    [SerializeField]
+    private float move = 1.0f;
+
+    [SerializeField]
+    private float moveSpeed = 0.5f;
+
+    //current timer on movement
+    [SerializeField]
+    private float counter = 0.0f;
+    //bound of movement
+    [SerializeField]
+    private float timer = 0.0f;
+
+    [SerializeField]
+    private bool isHorizontal = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (counter >= timer) { 
+            move = -move;
+            counter = 0.0f;
+        } else
+        {
+            counter += 0.1f;
+        }
+
+        if (isHorizontal) { 
+            XMove();
+        } else {
+            ZMove();
+        }
+
+    }
+
+    private void XMove() {
+        transform.position += new Vector3(move * moveSpeed * Time.deltaTime, 0.0f, 0.0f);
+    }
+
+    private void ZMove() {
+        transform.position += new Vector3(0.0f, 0.0f, move * moveSpeed * Time.deltaTime);
+    }
+
+}
